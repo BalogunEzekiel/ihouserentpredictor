823930b59303b51dead38130ca2cfe197c0fe2b8
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-06-09 17:11:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ input_df = pd.DataFrame({
 
 # Predict using the entire pipeline
 prediction = model.predict(input_df)[0]
-#    prediction = model.predict(input_df)
-    st.success(f"Estimated Rent: ₦{int(prediction[0]):,}")
+
+st.success(f"Estimated Rent: ₦{int(prediction[0]):,}")
