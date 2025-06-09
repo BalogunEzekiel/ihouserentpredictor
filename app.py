5bc52bc452494303ab8568b3463f77df6827330b
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-06-09 18:39:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,21 +22,30 @@ def user_input_features():
     toilets = st.sidebar.slider("Number of Toilets", 1, 5, 2)
     parking_space = st.sidebar.selectbox("Parking Space", [0, 1])
     year_added = st.sidebar.slider("Year Added", 2019, 2025, 2023)
+    
+    # Missing variables added here
+    house_type = st.sidebar.selectbox("House Type", ['Flat', 'Self-Contain', 'Duplex', 'Bungalow', 'Mini Flat', 'Other'])
+    lga = st.sidebar.selectbox("LGA", ['Ikeja', 'Eti-Osa', 'Yaba', 'Surulere', 'Ajah', 'Other'])
+    month_added = st.sidebar.slider("Month Added", 1, 12, 6)
+    day_added = st.sidebar.slider("Day Added", 1, 31, 15)
 
+    # Encode categorical fields if needed
     location_encoded = {'Ikeja': 0, 'Lekki': 1, 'Yaba': 2, 'Surulere': 3, 'Ajah': 4, 'Other': 5}[location]
+    house_type_encoded = {'Flat': 0, 'Self-Contain': 1, 'Duplex': 2, 'Bungalow': 3, 'Mini Flat': 4, 'Other': 5}[house_type]
+    lga_encoded = {'Ikeja': 0, 'Eti-Osa': 1, 'Yaba': 2, 'Surulere': 3, 'Ajah': 4, 'Other': 5}[lga]
 
     data = {
         'LOCATION': location_encoded,
         'BEDROOMS': bedrooms,
         'BATHROOMS': bathrooms,
         'TOILETS': toilets,
-        'HOUSE_TYPE': house_type,
-        'LGA': lga,
+        'HOUSE_TYPE': house_type_encoded,
+        'LGA': lga_encoded,
         'YEAR ADDED': year_added,
         'MONTH ADDED': month_added,
         'DAY ADDED': day_added
     }
-    
+
     return pd.DataFrame([data])
 
 # Get user input
